e99d446ef5bad23e4c048c13f20d27cc55aa6866
Shawn Deprey <shawndeprey@gmail.com>
2014-06-01 09:58:38 -0700

    Added Description meta and changed index title.

--- app/assets/javascripts/app-v1/routes/index.js
+++ app/assets/javascripts/app-v1/routes/index.js
@@ -1,9 +1,5 @@
 App.IndexRoute = App.BaseRoute.extend({
   model: function(){
     return this.store.createRecord('betalist');
-  },
-  setupController: function(controller, model) {
-    controller.set('model', model);
-    controller.get('controllers.application').setTitle('Innovative Discussion', false);
   }
 });
